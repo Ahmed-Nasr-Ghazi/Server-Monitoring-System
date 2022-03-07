643d19a62c72248c6198d08c3d0e486d7189ca59
Ahmed Nasr <a7med.nasr99@gmail.com>
2022-03-07 22:00:52 +0200

    Server entity updated

--- src/main/java/io/getarrays/server/model/Server.java
+++ src/main/java/io/getarrays/server/model/Server.java
@@ -1,8 +1,24 @@
 package io.getarrays.server.model;
 
+import io.getarrays.server.enumeration.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Server {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(unique = true)
+    @NotEmpty(message = "IP Address can't be empty or null")
     private String ipAddress;
     private String name;
     private String memory;
